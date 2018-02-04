1b68a6f9d96abb3546513776edd585d37934fbc5
vieiraa360 <vieiraa360@gmail.com>
2018-02-04 23:27:47 +0000

    update

--- static/js/graph.js
+++ static/js/graph.js
@@ -30,9 +30,6 @@ function makeGraphs(error, donorsUSProjects) {
     var povertyLevelDim = ndx.dimension(function (d) {
         return d["poverty_level"];
     });
-    var numDonors = nxd.dimention(function (d) {
-        return d["num_donors"];
-    });
     var stateDim = ndx.dimension(function (d) {
         return d["school_state"];
     });
@@ -45,7 +42,6 @@ function makeGraphs(error, donorsUSProjects) {
     var numProjectsByDate = dateDim.group();
     var numProjectsByResourceType = resourceTypeDim.group();
     var numProjectsByPovertyLevel = povertyLevelDim.group();
-    var numProjectsByNumDonors = numDonorsDim.group();
     var numProjectsByFundingStatus = fundingStatus.group();
     var totalDonationsByState = stateDim.group().reduceSum(function (d) {
         return d["total_donations"];
@@ -66,7 +62,6 @@ function makeGraphs(error, donorsUSProjects) {
     var timeChart = dc.lineChart("#time-chart");
     var resourceTypeChart = dc.rowChart("#resource-type-row-chart");
     var povertyLevelChart = dc.rowChart("#poverty-level-row-chart");
-    var numDonorsChart = dc.rowChart("#num-donors");
     var numberProjectsND = dc.numberDisplay("#number-projects-nd");
     var totalDonationsND = dc.numberDisplay("#total-donations-nd");
     var fundingStatusChart = dc.pieChart("#funding-chart");
@@ -132,13 +127,6 @@ function makeGraphs(error, donorsUSProjects) {
         .dimension(fundingStatus)
         .group(numProjectsByFundingStatus);
 
-    numDonorsChart
-        .ordinalColors(["#79CED7", "#66AFB2", "#C96A23", "#D3D1C5", "#F5821F"])
-        .width(300)
-        .height(250)
-        .dimension(numDonorsDim)
-        .group(numProjectsByNumDonors)
-        .xAxis().ticks(4);
 
 
     dc.renderAll();
